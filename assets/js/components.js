d1e8414988988845c924db98c49973e917b30779
CyBorg561 <binumer614@gmail.com>
2026-07-19 16:51:27 +0500

    Qorelix website update

--- assets/js/components.js
+++ assets/js/components.js
@@ -125,42 +125,9 @@ function initNavBehavior() {
   }, { passive: true });
 }
 
-/* --- Services Carousel --- */
-function initServicesCarousel() {
-  const container = document.querySelector('.services-carousel');
-  if (!container || typeof Swiper === 'undefined') return;
-
-  const swiper = new Swiper(container, {
-    slidesPerView: 'auto',
-    spaceBetween: 24,
-    loop: true,
-    speed: 600,
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-      pauseOnMouseEnter: true,
-    },
-    mousewheel: {
-      forceToAxis: true,
-    },
-    navigation: {
-      nextEl: '.services-carousel-next',
-      prevEl: '.services-carousel-prev',
-    },
-    breakpoints: {
-      320: { spaceBetween: 12 },
-      640: { spaceBetween: 16 },
-      1024: { spaceBetween: 24 },
-    },
-  });
-
-  container.swiper = swiper;
-}
-
 /* --- Initialize Components --- */
 document.addEventListener('DOMContentLoaded', () => {
   initTestimonialsCarousel();
-  initServicesCarousel();
   initClientsMarquee();
   initSmoothScrollLenis();
   initScrollRevealFallback();
